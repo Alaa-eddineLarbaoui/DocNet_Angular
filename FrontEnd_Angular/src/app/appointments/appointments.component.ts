e2178573a66f9c0f99d2c0379cd8cc91d70b24fe
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-09-07 23:46:45 +0100

    j'ai apporte des modifications sur le component Appointment

--- FrontEnd_Angular/src/app/appointments/appointments.component.ts
+++ FrontEnd_Angular/src/app/appointments/appointments.component.ts
@@ -11,16 +11,44 @@ export class AppointmentsComponent implements OnInit {
   availableTimes: string[] = [];
   selectedDate: string = '';
   patientId: number = 1;
+  minDate: string;
 
-  constructor(private appointmentService: AppointmentService) {}
+
+  constructor(private appointmentService: AppointmentService ) {
+    this.minDate = new Date().toISOString().split('T')[0];
+
+  }
 
   ngOnInit(): void {}
 
   loadAvailableTimes() {
     this.appointmentService.getAvailableTimes(this.selectedDate)
       .subscribe(times => {
-        this.availableTimes = times.map(time => time.slice(0, 5));
+
+        this.availableTimes = times.map(time => {
+
+          return time.slice(0, 5);
+        });
+      }, error => {
+        console.error('Erreur lors de la récupération des créneaux disponibles:', error);
       });
+  }
+
+  isWeekend(dateString: string): boolean {
+    if (!dateString) {
+      return false;
+    }
+    const date = new Date(dateString);
+    const day = date.getDay();
+    if (day === 0){
+      this.showAlert();
+      return true
+    }
+    return false ;
+  }
+
+  showAlert(): void {
+    window.alert("Le dimanche n'est pas disponible. Veuillez choisir un autre jour.");
   }
 
   reserveTime(time: string) {
