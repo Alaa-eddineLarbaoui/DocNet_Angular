a99f6e746561e70fe4d06c133c6b852aafb4420c
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-09-28 18:29:49 +0100

    j'ai ajoute une logique pour afficher les donnees d'un doctor

--- FrontEnd_Angular/src/app/doctor-details/doctor-details.component.ts
+++ FrontEnd_Angular/src/app/doctor-details/doctor-details.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {DoctorService} from "../Service/doctor.service";
+import {HealthProfessional} from "../Models/HealthProfessional";
 
 @Component({
   selector: 'app-doctor-details',
@@ -8,12 +10,26 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class DoctorDetailsComponent  implements OnInit{
   idProf !:number ;
+  doctorGet!: HealthProfessional;
   ngOnInit(): void {
     this.idProf = +this.route.snapshot.paramMap.get('id')!;
+    this.getDoctor();
 
   }
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private doctorService: DoctorService,
+  ) {
   }
+
+
+  getDoctor() {
+    this.doctorService.getHealthProfById(this.idProf).subscribe((doctor: HealthProfessional) => {
+      this.doctorGet = doctor;
+    });
+  }
+
+
 
 }
